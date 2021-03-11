a3f3f4b5f6b256a96b7883b27ed41642a11956d9
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-11 17:43:14 +0530

    [AKSHAY] added multiple contact

---
+++ Address Book New/AddContacts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class AddContacts
+    {
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Assign(string first_Name, string last_Name, string address, string city, string state, int zip, int phone_number, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Edit(string edit_First_Name, string edit_Last_Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void delete(string delete_First_Name, string delete_Last_Name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
